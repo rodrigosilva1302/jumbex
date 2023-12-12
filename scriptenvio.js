9a618c948d50620135af7ff1bd19c510cdd97c69
rodrigosilva1302 <145184599+rodrigosilva1302@users.noreply.github.com>
2023-12-12 17:57:37 -0300

    Add files via upload

---
+++ scriptenvio.js
@@ -0,0 +1,33 @@
+// Sua função para obter parâmetros da URL
+function obterParametroDaURL(nomeParametro) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(nomeParametro);
+}
+
+// Função para preencher os campos ocultos do formulário com os dados recuperados, incluindo a forma de pagamento
+function preencherCamposOcultos(formaPagamento) {
+    const detalhesCompra = obterParametroDaURL('detalhesCompra');
+    const nome = obterParametroDaURL('nome');
+    const sobrenome = obterParametroDaURL('sobrenome');
+    const whatsapp = obterParametroDaURL('whatsapp');
+
+    // Preencher os campos ocultos com os dados recuperados e a forma de pagamento
+    document.getElementById('detalhesCompraInput').value = detalhesCompra || '';
+    document.getElementById('nomeInput').value = nome || '';
+    document.getElementById('sobrenomeInput').value = sobrenome || '';
+    document.getElementById('whatsappInput').value = whatsapp || '';
+    document.getElementById('formaPagamentoInput').value = formaPagamento || '';
+}
+
+// Adicionar um identificador para os botões de pagamento (por exemplo, pix, cartão, outras)
+document.getElementById('pix').addEventListener('click', function() {
+    preencherCamposOcultos('Pix');
+});
+
+document.getElementById('cartão').addEventListener('click', function() {
+    preencherCamposOcultos('Cartão de Crédito');
+});
+
+document.getElementById('outras').addEventListener('click', function() {
+    preencherCamposOcultos('Outras formas de pagamento');
+});
